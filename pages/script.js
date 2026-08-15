008d5fcca628744683e3d608d18800063605db3b
Rose Liu <rose@byanyothername.me>
2026-08-15 13:17:14 -0400

    front end js

--- pages/script.js
+++ pages/script.js
@@ -1,28 +1,92 @@
+const devicesForm = document.getElementById("device-config");
 const deviceTableBody = document.getElementById("device-table-body");
+const devicesEndpoint = "/config/devices";
+
+devicesForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formSubmit();
+});
 
 var deviceCounter = 0;
+var existingDevices;
 
-function loadExisting() {
-    // get list of existing devices from server and call addDeviceRow for each
+loadExisting();
+
+async function loadExisting() {
+    resetDeviceTable();
+
+    try {
+        const response = await fetch(devicesEndpoint);
+        if (!response.ok) {
+            throw new Error(`HTTP error while fetching existing device data: ${response.status}`);
+        }
+        const result = await response.json();
+
+        for (const [key, value] of Object.entries(result)) {
+            addDeviceRow(key, value);
+        }
+        existingDevices = result;
+    } catch (error) {
+        console.error("Failed to load device data", error);
+    }
 }
 
 function addDeviceRow(id, device) {
     let row = document.createElement("tr");
 
     if (!id) {
-        addNewInputCell("text", "id", id, "table-heading-id");
+        row.dataset.id = "";
+        addNewInputCell("text",
+            "id-" + deviceCounter,
+            "",
+            "table-heading-id",
+            { onchange: "validateNewId(this)" }
+        );
     }
     else {
+        row.dataset.id = id;
         addNewStaticCell(id);
     }
 
-    addNewInputCell("text", "name-" + deviceCounter, device.name, "table-heading-name");
-    addNewInputCell("number", "width-" + deviceCounter, device.width, "table-heading-width");
-    addNewInputCell("number", "height-" + deviceCounter, device.height, "table-heading-height");
-    addNewInputCell("checkbox", "preferbmp-" + deviceCounter, device.prefer_bmp, "table-heading-preferbmp");
-    addNewInputCell("number", "grayscale-" + deviceCounter, device.grayscale_depth, "table-heading-grayscale");
-    addNewInputCell("number", "color-" + deviceCounter, device.color_depth, "table-heading-color");
-    addNewInputCell("text", "url-" + deviceCounter, device.url, "table-heading-url");
+    addNewInputCell("text",
+        "name-" + deviceCounter,
+        device.name,
+        "table-heading-name"
+    );
+    addNewInputCell("number",
+        "width-" + deviceCounter,
+        device.width,
+        "table-heading-width",
+        { min: 1, step: 1 }
+    );
+    addNewInputCell("number",
+        "height-" + deviceCounter,
+        device.height,
+        "table-heading-height",
+        { min: 1, step: 1 }
+    );
+    addNewInputCell("checkbox",
+        "preferbmp-" + deviceCounter,
+        device.prefer_bmp,
+        "table-heading-preferbmp"
+    );
+    addNewInputCell("number",
+        "grayscale-" + deviceCounter,
+        device.grayscale_depth,
+        "table-heading-grayscale",
+        { min: 1, step: 1, max: 16 }
+    );
+    addNewInputCell("number",
+        "color-" + deviceCounter,
+        device.color_depth,
+        "table-heading-color",
+        { min: 0, step: 1, max: 16 }
+    );
+    addNewInputCell("url",
+        "url-" + deviceCounter,
+        device.url,
+        "table-heading-url"
+    );
 
     addNewStaticCell(device.api_key);
     
@@ -37,14 +101,20 @@ function addDeviceRow(id, device) {
         row.appendChild(cell);
     }
 
-    function addNewInputCell(inputType, fieldName, value, colHeadId) {
+    function addNewInputCell(inputType, fieldName, value, colHeadId, otherAttributes) {
         let cell = document.createElement("td");
-        cell.innerHTML = `
-            <input type="${inputType}"
+        let attributes = `type="${inputType}"
                 name="${fieldName}"
                 value="${value}"
-                aria-labelledby="${colHeadId}">
-        `;
+                aria-labelledby="${colHeadId}"`;
+        
+        if (otherAttributes) {
+            for (const [key, value] of Object.entries(otherAttributes)) {
+                attributes += ` ${key}="${value}"`;
+            }
+        }
+        
+        cell.innerHTML = `<input ${attributes}">`;
 
         row.appendChild(cell);
     }
@@ -56,18 +126,82 @@ function resetDeviceTable() {
 }
 
 function addNewDevice() {
-    addDeviceRow("", {
+    addDeviceRow(null, {
         name: "New Device",
         width: 800,
         height: 480,
         prefer_bmp: false,
         grayscale_depth: 4,
         color_depth: 0,
-        url: constants.WELCOME_PAGE_ABS_URL,
+        url: "https://en.wikipedia.org/wiki/Main_Page",
         api_key: ""
     });
 }
 
-function validateRow() {
+function validateNewId(el) {
+    const id = el.value;
+    let isValid = true;
 
+    if (existingDevices.hasOwnProperty(id)) isValid = false;
+    else {
+        let newIdFields = deviceTableBody.querySelectorAll("input[name^='id-']");
+        for (const field of newIdFields) {
+            if (field !== el && field.value == id) {
+                isValid = false;
+                break;
+            }
+        }
+    }
+
+    if (isValid) {
+        el.setCustomValidity("IDs must be unique per device! It is recommended to use the device's MAC address");
+    } else {
+        el.setCustomValidity("");
+    }
+}
+
+function formSubmit() {
+    const rows = deviceTableBody.querySelectorAll("tr");
+    let devices = { ...existingDevices };
+    
+    rows.forEach((row) => {
+        let id = row.dataset.id;
+        if (!id) {
+            id = getFieldValue(row, "id");
+            devices[id] = {};
+        }
+
+        let device = devices[id];
+        device[name] = getFieldValue(row, "name");
+        device[width] = getFieldValue(row, "width");
+        device[height] = getFieldValue(row, "height");
+        device[prefer_bmp] = getFieldValue(row, "preferbmp");
+        device[grayscale_depth] = getFieldValue(row, "grayscale");
+        device[color_depth] = getFieldValue(row, "color");
+        device[url] = getFieldValue(row, "url");
+    });
+
+    try {
+        const response = await fetch(devicesEndpoint, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(devices)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+    } catch(error) {
+        console.error("Error saving device data", error);
+    }
+
+    console.log("New device data successfully saved!");
+
+    loadExisting();
+
+    function getFieldValue(row, fieldName) {
+        let field = row.querySelector(`input[name^="${fieldName}-"]`);
+        if (field.type == "checkbox") return field.checked;
+        return field.value;
+    }
 }
