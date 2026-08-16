7a6fd6f8f46d79b507096a5a0f9a9ebdde55c3f5
Rose Liu <rose@byanyothername.me>
2026-08-16 03:42:20 -0400

    asdf

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function startServer() {
             if (renderedFilename) {
                 res.json({
                     filename: renderedFilename,
-                    image_url: `${constants.RENDERS_ABS_URL}/${renderedFilename}?t=${Temporal.Now.instant().epochMilliseconds}`
+                    image_url: `${constants.RENDERS_ABS_URL}/${renderedFilename}?t=${Date.now()}`
                 });
                 return;
             }
